bd8ae05e531a07b15a761ce82c2362da135d428f
Drowning Cat <shamal.ma.personal@gmail.com>
2024-07-22 19:23:01 +0300

    fix: remove annoying space

--- init.lua
+++ init.lua
@@ -581,7 +581,7 @@ require('lazy').setup({
 
         lua_ls = {
           -- cmd = {...},
-          -- filetypes = { ...},
+          -- filetypes = {...},
           -- capabilities = {},
           settings = {
             Lua = {
